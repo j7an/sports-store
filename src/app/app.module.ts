35ef739848d34c6cb822543cb5d5019df8d691e6
Jia Tan <j7an@users.noreply.github.com>
2017-05-02 19:23:37 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    StoreModule
+    // FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
