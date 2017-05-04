1a09154780cd33b0bd77ff137d8c2dbd879ee51f
Jia Tan <j7an@users.noreply.github.com>
2017-05-04 07:47:52 -0700

    cart checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from "./store/store.module";
+import { StoreComponent } from "./store/store.component";
+import { CheckoutComponent } from "./store/checkout.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { RouterModule } from "@angular/router";
+import { StoreFirstGuard } from "./storeFirst.guard";
 
 @NgModule({
   declarations: [
@@ -12,11 +17,17 @@ import { StoreModule } from "./store/store.module";
   ],
   imports: [
     BrowserModule,
-    StoreModule
+    StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent, canActivate: [StoreFirstGuard] },
+      { path: "cart", component: CartDetailComponent, canActivate: [StoreFirstGuard] },
+      { path: "checkout", component: CheckoutComponent, canActivate: [StoreFirstGuard] },
+      { path: "**", redirectTo: "/store" }
+    ])
     // FormsModule,
     // HttpModule
   ],
-  // providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
